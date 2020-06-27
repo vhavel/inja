0045084bc4eb4b0d1e7f3672843bc9a2721e0fc8
pantor <pantor@users.noreply.github.com>
2020-06-27 15:51:16 +0000

    Automated deployment: Sat Jun 27 15:51:16 UTC 2020 dadbb856cd95beb938fd3e7bd4e79f35bb309287

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['renderconfig',['RenderConfig',['../structinja_1_1_render_config.html',1,'inja']]],
   ['renderer',['Renderer',['../classinja_1_1_renderer.html',1,'inja']]],
   ['rendererror',['RenderError',['../structinja_1_1_render_error.html',1,'inja']]]
 ];
