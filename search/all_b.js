b835cfd23789ca0156c5c13cf038858f901eeb5b
pantor <pantor@users.noreply.github.com>
2020-08-03 19:33:20 +0000

    Automated deployment: Mon Aug  3 19:33:20 UTC 2020 889da4da4fc48fc23b9b449bad202c265d0e0449

--- search/all_b.js
+++ search/all_b.js
@@ -8,6 +8,7 @@ var searchData=
   ['set_5fstatement',['set_statement',['../classinja_1_1_environment.html#a3de73df9dc768bd00633a41a58fb5d41',1,'inja::Environment']]],
   ['set_5fthrow_5fat_5fmissing_5fincludes',['set_throw_at_missing_includes',['../classinja_1_1_environment.html#a3306a8128013daff4e00b5b55e84f995',1,'inja::Environment']]],
   ['set_5ftrim_5fblocks',['set_trim_blocks',['../classinja_1_1_environment.html#a6825801f1d4b98728b71056dddf44a85',1,'inja::Environment']]],
+  ['setstatementnode',['SetStatementNode',['../classinja_1_1_set_statement_node.html',1,'inja']]],
   ['sourcelocation',['SourceLocation',['../structinja_1_1_source_location.html',1,'inja']]],
   ['statementnode',['StatementNode',['../classinja_1_1_statement_node.html',1,'inja']]],
   ['statisticsvisitor',['StatisticsVisitor',['../classinja_1_1_statistics_visitor.html',1,'inja']]],
--- search/classes_a.js
+++ search/classes_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['setstatementnode',['SetStatementNode',['../classinja_1_1_set_statement_node.html',1,'inja']]],
   ['sourcelocation',['SourceLocation',['../structinja_1_1_source_location.html',1,'inja']]],
   ['statementnode',['StatementNode',['../classinja_1_1_statement_node.html',1,'inja']]],
   ['statisticsvisitor',['StatisticsVisitor',['../classinja_1_1_statistics_visitor.html',1,'inja']]]
